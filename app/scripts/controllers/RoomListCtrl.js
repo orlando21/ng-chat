8f38db285973c790b47aa97f1d1bc5562c49850c
Greg Babb <info@textlogic.ch>
2016-04-11 05:17:01 +0200

    Add ability to list messages for chat rooms

--- app/scripts/controllers/RoomListCtrl.js
+++ app/scripts/controllers/RoomListCtrl.js
@@ -1,7 +1,20 @@
 (function() {
-    function RoomListCtrl($rootScope, Room, $modal, Message) {
+    function RoomListCtrl($rootScope, Room, $modal) {
         $rootScope.rooms=Room.all;
-//        $rootScope.activeRoom = $rootScope.rooms(-KEqMcaaOFShRR5tuKBP);
+
+        $rootScope.rooms.$loaded().then(function(rooms, newRoomID) {
+
+            // initialize by setting activeRoom to first chat room
+            $rootScope.activeRoom = $rootScope.rooms[0];
+            $rootScope.roomMessages = Room.getMessages(rooms[0].$id);
+
+            // setRoom() when user selects another chat room
+            $rootScope.setRoom = function(newRoomID) {
+                $rootScope.activeRoom = Room.getRoom(newRoomID);
+                $rootScope.roomMessages = Room.getMessages(newRoomID);
+            };
+
+        });
 
         // opens the 'create_room_modal' modal
         $rootScope.openRoomModal = function() {
@@ -12,22 +25,10 @@
             });
         };
 
-        $rootScope.setRoom = function() {
 
-        };
-/*
-        // set room title & content <in dev>
-        $rootScope.setRoom = function(room) {
-            $rootScope.rooms.getMessages(room.$id, function (messages) {
-                $rootScope.messages = messages;
-                $rootScope.$apply();
-                $rootScope.roomId = room.$id;
-            });
-        };
-*/
     }
 
     angular
         .module('blocChat')
-        .controller('RoomListCtrl', ['$rootScope', 'Room', '$modal', 'Message', RoomListCtrl]);
+        .controller('RoomListCtrl', ['$rootScope', 'Room', '$modal', RoomListCtrl]);
 })();
